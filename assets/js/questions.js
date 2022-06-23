43bc0d12c85f7cd9b5cba1f42d5e20d90da96749
Jonathan Thompson <jathompson399@gmail.com>
2022-06-22 21:33:48 -0500

    created questions

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "How would you log variable X in the console?"
+        multiChoice: ["log(x)", "console.log(x)", "console(x)", "logConsole(x)"]
+        answer: "console.log(x)"
+    },
+    {
+        title: "What does && means?"
+        multiChoice: ["and", "plus", "or", "yes" ]
+        answer: "and"
+    },
+    {
+        title: "What does || mean?"
+        multiChoice: ["or", "no", "minus", "and"]
+        answer: "or"
+    },
+    {
+        title: "What does 'var run = function()' mean?"
+        multiChoice: ["run the variable function", "the variable, run, is a function", "function variables run", "go for a run"]
+        answer: "the variable, run, is a function"
+    },
+    {
+        title: "What Event Listener accepts a enter keystroke or a mouse click?"
+        multiChoice: ["submit", "click", "enter", "accept"]
+        answer: "submit"
+    },
+]
